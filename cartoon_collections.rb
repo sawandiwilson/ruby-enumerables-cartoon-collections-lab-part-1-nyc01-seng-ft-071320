9f084a110d56c98c218807d28a0653f2ab8e031a
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-06 16:58:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def greet_characters(array)
   characters_array = ["Hoggle", "Ludo", "Sir Didymus"]
+characters_array.each do |characters_array|
+   puts "#{oppressed_worker.capitalize} wants to start a union!"
+end 
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
